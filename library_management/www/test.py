2f0b7dc870cd45a981096fb8eb419a2636f71272
Veerabala <veerabala.maruthaiyan@rapidqube.com>
2022-12-16 18:29:55 +0530

    html pages

--- library_management/www/test.py
+++
@@ -1,6 +0,0 @@
-import frappe 
-
-def get_context(context):
-    items = frappe.db.sql(""" SELECT article,full_name,type FROM `tabLibrary Transaction`;""",as_dict=True)
-    context.items = items
-    return context
