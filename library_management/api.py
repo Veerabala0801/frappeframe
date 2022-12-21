01ff4e626405bde1222b1a1f08559f50bcba51e0
Veerabala <veerabala.maruthaiyan@rapidqube.com>
2022-12-21 12:34:58 +0530

    hii

--- library_management/api.py
+++ library_management/api.py
@@ -10,28 +10,27 @@ def insert_user(first_name,email):
         "first_name":first_name,
         "email":email,
         "new_password":"Frappe@123",
-        "role_profile_name": "RPQB",
-        "user_type":"System User"
+        "role_profile_name":"RPQB",
+        "user_type":"System User",
+        "module_profile":"RPQB-Man"
         })
     user.insert(ignore_permissions=True)    
     frappe.db.commit()
-    
-    
-    # user_ins = frappe.db.sql(""" update `tabUser` set  role_profile_name = {} ,redirect_url = {} where email = {}""".format(role,url,email))
-    # user_ins.insert(ignore_permissions=True)
-    # frappe.db.commit()
-    return f"Value {first_name} and {email} inserted"
-
+   
+    return f"The data for {first_name} inserted"
+#thalapathi
 # @frappe.whitelist()
 # def user_role(module,role,url):
-# 
 #    u_role = frappe.get_doc({"doctype":"User","module_profile":module,"role_profile_name":role,"redirect_url":url})
 #    u_role.update(ignore_permissions=True)
 #    frappe.db.commit()
 #    return "Value Insterted"
+#  user_ins = frappe.db.sql(""" update `tabUser` set  role_profile_name = {} ,redirect_url = {} where email = {}""".format(role,url,email))
+#     user_ins.insert(ignore_permissions=True)
+#     frappe.db.commit()
 @frappe.whitelist()
-def get_role_user(role):
-    roles= frappe.get_doc({"doctype":"User","role_profile":role})
+def user_module(role):
+    roles= frappe.get_doc({"doctype":"User","":role})
     roles.insert(ignore_permissions=True)
     frappe.db.commit()
     return "success"
